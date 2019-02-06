ea262835a9c62e3545fc34447e4e9e0808e4d995
Charos12 <andrew.le12@gmail.com>
2019-02-05 21:01:11 -0500

    JSON added

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    <div className="card" onClick={() => props.removeFriend(props.id)}>
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content" />
+    </div>
+  );
+}
+
+export default FriendCard;
