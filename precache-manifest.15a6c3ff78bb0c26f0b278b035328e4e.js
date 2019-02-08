91c912a718ead702c8a35771b41c2bd64bd4e482
Charos12 <andrew.le12@gmail.com>
2019-02-08 17:36:24 -0500

    Updates

---
+++ precache-manifest.15a6c3ff78bb0c26f0b278b035328e4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff1fe26e938af34ae615",
+    "url": "/yugiohclickygame/static/js/runtime~main.ff1fe26e.js"
+  },
+  {
+    "revision": "1967d4d3c82a2261589d",
+    "url": "/yugiohclickygame/static/js/main.1967d4d3.chunk.js"
+  },
+  {
+    "revision": "37672ba115e8d3560bc2",
+    "url": "/yugiohclickygame/static/js/1.37672ba1.chunk.js"
+  },
+  {
+    "revision": "1967d4d3c82a2261589d",
+    "url": "/yugiohclickygame/static/css/main.cf1dda55.chunk.css"
+  },
+  {
+    "revision": "15be7f5c68c8a831e265d1a2e15ecfd2",
+    "url": "/yugiohclickygame/index.html"
+  }
+];
