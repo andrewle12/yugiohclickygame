c44cb987116957e0ae8f8c6aa3e5b8b01ca7e459
Charos12 <andrew.le12@gmail.com>
2019-02-08 17:07:01 -0500

    Title CSS

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,6 +5,7 @@ function Title(props) {
   return (
     <h3 className="title">
       <span className="clicky">YuGiOh! Clicky Game</span>
+      <div id="message"> Try not to click the same card more than once!</div>
       {}
     </h3>
   );
